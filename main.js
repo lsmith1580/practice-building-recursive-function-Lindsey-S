7b95740c0ddc92fe822c6f7974a967c195ff1c45
Lindsey Smith <lindseysmith1580@gmail.com>
2025-04-10 09:50:43 -0500

    Added notes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function reverseString(string) {
     return string.charAt(string.length - 1) + reverseString(string.slice(0, string.length - 1));
 }
 
+
 console.log(reverseString("Goodbye"));
 
 console.log(reverseString("Radar"));
@@ -12,3 +13,7 @@ console.log(reverseString("Radar"));
 console.log(reverseString("Elbow"));
 
 console.log(reverseString("z"));
+
+let stringTest = "Hello"
+
+console.log(stringTest.charAt(stringTest.length - 1) + stringTest.slice(0, stringTest.length - 1));
